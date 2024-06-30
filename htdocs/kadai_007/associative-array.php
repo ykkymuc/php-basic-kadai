8d5b525280c9461320e6010df5c9081fcfc5df7e
ykkymuc <161896268+ykkymuc@users.noreply.github.com>
2024-06-30 21:40:35 +0900

    Add files via upload

---
+++ htdocs/kadai_007/associative-array.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html lang="ja">
+
+<head>
+  <meta charset="UTTF-8">
+  <title>連想配列を作って値とキーを出力しよう</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    // 連想配列に値を代入する
+    $personal_data = ['name' => 'onion', 'price' => 200, 'weight' => '160'];
+
+    // 連想配列の値を出力する
+    print_r($personal_data);
+
+    ?>
+  </p>
+  
+
+</body>
+
+
+</html>
